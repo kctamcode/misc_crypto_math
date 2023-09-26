6698a3a6a79fb185da2596cb6bba1e07ca9a0216
kctamcode <143941692+kctamcode@users.noreply.github.com>
2023-09-26 21:31:22 +0800

    Update barycentric_lagrange_interpolation.py

--- barycentric_lagrange_interpolation.py
+++ barycentric_lagrange_interpolation.py
@@ -34,22 +34,6 @@ def fp_div_polys(a, b, p: int):
         diff -= 1
     return [x % p for x in o]
 
-'''
-def lagrange_polynomial(mdata: list, p: int):
-    x_coords = [mdata[i][0] for i in range(len(mdata))]
-    y_coords = [mdata[i][1] for i in range(len(mdata))]
-    base_poly = [0]
-    for i in range(len(x_coords)):
-        basis = [1]
-        for j in range(len(x_coords)):
-            if j == i:
-                continue
-            basis = poly_mul_modp(basis, [-x_coords[j], 1], p)
-            basis = poly_rmul_modp(basis, fp_inv(x_coords[i] - x_coords[j], p), p)
-        base_poly = poly_add(base_poly, poly_rmul_modp(basis, y_coords[i], p))
-    return [coef %p for coef in base_poly]
-'''
-
 def padding_zero(lst: list, itr: int):
     return [lst[i] if i in range(len(lst)) else 0  for i in range(len(lst) + max(itr, 0))]
 
@@ -82,22 +66,6 @@ def z_poly_modp(ind_list: list, p: int):
     assert len(zp) == len(ind_list) + 1
     return list(zp)
 
-'''
-def z_poly_modp_np(ind_list: list, p: int):
-    zp = np.poly1d([1])
-    for i in ind_list:
-        zp = np.polymul(zp, np.poly1d([-i, 1])) #too large anyway
-        zp = np.poly1d([zp[i]%p for i in range(len(zp))])
-    #assert len(zp) == len(ind_list)+1
-    return list(zp)
-'''
-
-'''
-def fp_div_polys_np(a, b, p): # slower than above
-    qp = np.polydiv(a[::-1], b[::-1])[0]
-    return [int(qp[i])%p for i in range(len(qp))][::-1]
-'''
-
 def z_inv_modp(ind_list: list, t: int, p: int):
     zp = [1]
     for i in range(t):
@@ -113,11 +81,6 @@ def gen_ranindlst(lngth: int, rnge: int):
     temlst = list(temp)
     temlst.sort()
     return temlst
-
-'''
-def diff_modp(poly: list, p: int):
-    return [i*poly[i+1] %p for i in range(len(poly)-1)]
-'''
 
 def poly_sum_modp(poly_list: list, p: int):
     ps = [0]
@@ -159,37 +122,3 @@ P2 = poly_sum_modp(d2_list, p)
 t3=time.time()
 print('interpolation: ', t3-t2)
 
-'''
-# test part
-for i in range(len(indice)//4):
-    if poly_eval_modp(P2, indice[i], p) != corr_y[i]:
-        print('bugs', i)
-'''
-
-'''
-t1=time.time()
-indice = gen_ranindlst(l, p-1)
-corr_y = [random.randint(1, p-1) for i in range(l)]
-
-A_poly = z_poly_modp(indice, p)
-Ap_poly = diff_modp(A_poly, p)
-indice_list = [indice[:i] + indice[i+1:] for i in range(l)] #len correct
-A_poly_list = [fp_div_polys(A_poly, [-indice[i]%p, 1], p) for i in range(len(indice))]
-t2=time.time()
-
-#t1=time.time()
-#A_poly_list = [z_poly_modp(indice_list[i], p) for i in range(l)]
-#A_poly_list = [fp_div_polys(A_poly, [-indice[i], 1], p) for i in range(len(indice))]
-#A_poly_list_np = [fp_div_polys(A_poly, [-indice[i]%p, 1], p) for i in range(len(indice))]
-
-c_list = [corr_y[i]* fp_inv(poly_eval_modp(Ap_poly, indice[i], p),p) %p for i in range(len(indice))]
-d_list = [poly_rmul_modp(A_poly_list[i], c_list[i], p) for i in range(len(indice))]
-P = poly_sum_modp(d_list, p)
-t3=time.time()
-
-for i in range(len(indice)):
-    if poly_eval_modp(P, indice[i], 1033) != corr_y[i]:
-        print('bugs')
-        break
-print('done')
-'''
